306a73e83af4c016adc7b886c5fcec27aa10d485
MARCO CEBRIAN MARIRRODRIGA <marcocebrian55@gmail.com>
2026-01-30 11:32:25 +0000

    guardado

--- src/models.py
+++ src/models.py
@@ -1,19 +1,86 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey, Table, Column, Integer, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 
 db = SQLAlchemy()
 
+favorites_characters = Table(
+    "favorites_characters",
+    db.metadata,
+    Column("user_id", Integer, ForeignKey("user.id"), primary_key=True),
+    Column("character_id", Integer, ForeignKey("character.id"), primary_key=True)
+)
+
+favorites_locations = Table(
+    "favorites_locations",
+    db.metadata,
+    Column("user_id", Integer, ForeignKey("user.id"), primary_key=True),
+    Column("location_id", Integer, ForeignKey("location.id"), primary_key=True)
+)
+
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    firstname: Mapped[str] = mapped_column(String(120), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(120), nullable=False)
+    favorites_characters:Mapped[list["Character"]]=relationship(
+        "Character",
+        secondary = favorites_characters,
+        back_populates= "favorite_by_user"
+    )
 
+    favorites_locations:Mapped[list["Location"]]=relationship(
+        "Location",
+        secondary = favorites_locations,
+        back_populates= "favorite_by_user"
+    )
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname
+        }
+
+
+class Character(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+    favorite_by_user:Mapped[list["User"]]=relationship(
+        "User",
+        secondary = favorites_characters,
+        back_populates= "favorites_characters"
+    )
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
+
+
+class Location(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+    favorite_by_user:Mapped[list["User"]]=relationship(
+        "User",
+        secondary = favorites_locations,
+        back_populates= "favorites_locations"
+    )
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
         }
